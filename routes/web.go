a162d4011a65d203b4dea658bc7a8d5fe5a8e90c
WYH <wangzhezd@gmail.com>
2018-11-26 13:13:36 +0800

    Add adminlte template demo

--- routes/web.go
+++ routes/web.go
@@ -9,11 +9,17 @@ import (
 
 func RegisterWebRoutes(env *server.Env) {
 	router := env.Gin
-	router.LoadHTMLGlob("templates/*")
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "resources/static")
 
+	router.GET("/nav", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "top-nav.tmpl.html", gin.H{
+			"title": "Main website",
+		})
+	})
 	// Default HTML page (client-side routing implemented via Vue.js)
 	router.NoRoute(func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
 			"title": "Main website",
 		})
 	})
