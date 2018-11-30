ebefe03d8e109d4dec27822c0e52fce72109556c
WYH <wangzhezd@gmail.com>
2018-11-30 18:07:33 +0800

    Add new teplate engine

--- routes/web.go
+++ routes/web.go
@@ -3,13 +3,30 @@ package routes
 import (
 	"goWeb/server"
 	"net/http"
+	"time"
 
+	"html/template"
+
+	"github.com/foolin/gin-template"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterWebRoutes(env *server.Env) {
 	router := env.Gin
-	router.LoadHTMLGlob("templates/*.tmpl.html")
+	//new template engine
+	router.HTMLRender = gintemplate.New(gintemplate.TemplateConfig{
+		Root:      "views",
+		Extension: ".tpl.html",
+		Master:    "layouts/master",
+		// Partials:  []string{"partials/ad"},
+		Funcs: template.FuncMap{
+			"copy": func() string {
+				return time.Now().Format("2006")
+			},
+		},
+		DisableCache: true,
+	})
+	// router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "resources/static")
 
 	router.GET("/nav", func(c *gin.Context) {
@@ -19,7 +36,7 @@ func RegisterWebRoutes(env *server.Env) {
 	})
 	// Default HTML page (client-side routing implemented via Vue.js)
 	router.NoRoute(func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
+		c.HTML(http.StatusOK, "index", gin.H{
 			"title": "Main website",
 		})
 	})
